e11314ede1f7025570ef3ca1d0fc5a7cec14c87c
Danilo Franco <engdanilofranco@gmail.com>
2023-04-25 12:21:33 -0300

    Primeiro commit do projeto de kit de de ferramntas para motoristas de app

---
+++ README.md
@@ -0,0 +1,9 @@
+kit de ferramentas para auxilio de motoristas de app.
+
+Inicialmente desenvolvido em HTML, CSS e javascrip.
+
+Inicialmente o kit de ferramentas para auxilio de motoristas de app conta com os seguintes moódulos:
+    1. Escolha do melhor combustível
+    2. Calculadora de custo por km
+    3. Planejador de metas
+    
---
+++ src/caluloDecustoPorKM.js
@@ -0,0 +1,22 @@
+function calcularCustoPorKm(){ 
+    
+    event.preventDefault();
+
+    let consumoNagasolina = Number(document.getElementById('consumoGasolina').value);
+    let consumoNoEtanol = Number(document.getElementById('consumoEtanol').value);
+    let valorGasolina = Number(document.getElementById('valorGasolina').value);
+    let valorEtanol = Number(document.getElementById('valorEtanol').value);
+
+    //calculo de rendimento
+    let rendimentoNagasolina = (1/consumoNagasolina).toFixed(3);
+    let rendimentoNoEtanol = (1/consumoNoEtanol).toFixed(3);
+
+    //calculo de custo por km
+    let custoNagasolina = (rendimentoNagasolina * valorGasolina).toFixed(2);
+    let custoNoEtanol = (rendimentoNoEtanol * valorEtanol).toFixed(2);   
+    
+
+    let newResultado = document.getElementById('resultado');
+    newResultado.innerHTML += `O rendimento do seu carro na gasolina é ${rendimentoNagasolina} km/L</br> O rendimento do seu carro no etanol é ${rendimentoNoEtanol} km/L</br> O seu custo por Km utilizando Gasolina é ${custoNagasolina} R$/Km</br> O seu custo por km utilizando Etanol é ${custoNoEtanol} R$/km`;
+
+}
